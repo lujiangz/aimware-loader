a998239464101e38eedff924c612ba3eb838815a
lujiangz <31911561+lujiangz@users.noreply.github.com>
2019-09-08 11:03:13 +0300

    Add files via upload

---
+++ v1go/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace v1go
+{
+    /**
+    * This is a recreation of the AIMWARE Loader, made by aequabit.
+    * Please don't steal this, it was a lot of work.
+    */
+    
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Loader("adolf"));
+            Application.Run(new PleaseWait());
+        }
+    }
+}
